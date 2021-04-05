c56e334aac16936c9ba89dc49159abd7c844740d
sanmichukwuka <michaelsanmi09@gmail.com>
2021-04-05 08:22:25 -0500

    replaced SpecialNotes.js with CompanyInfo.js

---
+++ src/page-section/book-now/CompanyInfo.js
@@ -0,0 +1,58 @@
+
+import React, { useState } from "react";
+
+export default function CompanyInfo({ handleNextSection }) {
+  const [organisation, setOrganisation] = useState({ name: "", email: "" });
+
+  const onTextChange = (event, type) =>
+    setOrganisation({ ...organisation, [type]: event.target.value });
+
+    console.log(organisation);
+
+  return (
+    <div class=" mt-5 d-flex justify-content-center">
+      <div className="position-relative start-40">
+        <h1 className="text-primary text-bold">Organization details.</h1>
+        <form className="mt-3">
+          <div className="me-3">
+            <label for="basic-url" class="form-label">
+              Organization Name{" "}
+            </label>
+            <input
+              onChange={(event) => onTextChange(event, "name")}
+              placeholder="Organization Name"
+              type="text"
+              class="form-control"
+              aria-label="Dollar amount (with dot and two decimal places)"
+            />
+          </div>
+          <div className="me-3">
+            <label for="basic-url" class="form-label">
+              Company Email Address
+            </label>
+            <input
+              onChange={(event) => onTextChange(event, "email")}
+              placeholder="Company Email"
+              type="email"
+              class="form-control"
+              aria-label="Dollar amount (with dot and two decimal places)"
+            />
+          </div>
+
+          <button
+            type="button"
+            className="btn mt-4 btn-lg btn-primary"
+            // onChange={(event) => onTextChange(event, "email")}
+            onClick={() => handleNextSection("organisation", organisation)}
+          >
+            NEXT
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+
+
+
